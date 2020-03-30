83141b89098f9da0ad9cda10db15f10c2d0f8e75
Storh <storh1118@outlook.com>
2020-03-30 11:20:50 +0800

    统一错误处理

--- app/middleware/jwt_error_handler.js
+++ app/middleware/jwt_error_handler.js
@@ -4,7 +4,12 @@ module.exports = (options, app) => {
   return async (ctx, next) => {
     try {
       await next();
+      ctx.body = {
+        error: 0,
+        message: '',
+      };
     } catch (err) {
+      ctx.app.emit('error', err, ctx);
       // console.log(err);
       if (
         ctx.path.includes('/') &&
@@ -14,10 +19,17 @@ module.exports = (options, app) => {
         ctx.body = {
           error: -1, // 无权限
         };
-        return;
+        // return;
+      } else {
+        ctx.status = 200;
+        ctx.body = {
+          error: 1,
+          message: '数据更新错误',
+          data: {},
+        };
       }
-      throw err;
+      // return;
+      // throw err;
     }
-
   };
 };
