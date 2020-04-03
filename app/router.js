53712eb352fe1ac6f340b559873c33bed4a7caa5
Storh <storh1118@outlook.com>
2020-04-03 12:00:55 +0800

    接口注释

--- app/router.js
+++ app/router.js
@@ -8,11 +8,11 @@ module.exports = app => {
   router.get('/', controller.home.index);
   router.post('/jwttest', app.jwt, controller.member.phoneLogin.jwttest);
 
-  router.post('/app/member/phoneLogin/bindPhone', controller.member.phoneLogin.bindPhone);
-  router.post('/app/member/info/reg', app.jwt, controller.member.info.reg);// 注册后完善用户信息
+  router.post('/app/member/phoneLogin/bindPhone', controller.member.phoneLogin.bindPhone);// 1.1.3、 微信绑定手机号
+  router.post('/app/member/info/reg', app.jwt, controller.member.info.reg);// 1.1.6、 注册后完善用户信息
 
   router.post('/app/member/info/getInfo', app.jwt, controller.member.info.getInfo);// 1.5.1、 获取用户基本信息
 
-  router.post('/login', controller.member.phoneLogin.login);
-  router.post('/app/baseData/distList', app.jwt, controller.baseData.distList);// 获取小区列表
+  router.post('/login', controller.member.phoneLogin.login);// jwt测试接口
+  router.post('/app/baseData/distList', app.jwt, controller.baseData.distList);// 1.6.1、 获取街道小区列表
 };
