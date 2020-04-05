0d20d7b5d10376a47394b1e1429b3c626b64377e
Storh <storh1118@outlook.com>
2020-04-05 10:43:10 +0800

    整理文件

--- app/controller/member/phoneLogin.js
+++ app/controller/member/phoneLogin.js
@@ -13,30 +13,7 @@ class PhoneLoginController extends Controller {
       this.ctx.throw('登录数据不完整');
     }
   }
-  async gerReturnData(user_id) {
-    const raw_info = await this.app.mysql.get('userinfo', { user_id });
-    const user_info = JSON.parse(JSON.stringify(raw_info));
-    const sign_user_info = {};
-    sign_user_info.user_id = user_info.user_id;
-    sign_user_info.nickname = user_info.nickname;
-    sign_user_info.headimgurl = user_info.headimgurl;
-    sign_user_info.need_info = 1;// 需要完善信息
-    if (user_info.districts) {
-      sign_user_info.need_info = 0;// 不需要完善信息
-    }
-    return sign_user_info;
-  }
-  async jwttest() {
-    const { ctx, app } = this;
-    const abody = ctx.state.user;
-    console.log(abody);
-    const token = app.jwt.sign({ foo: 'bar' }, app.config.jwt.secret, { expiresIn: '3 days' });
-    // const token = app.jwt.sign({ foo: 'bar' }, app.config.jwt.secret, { expiresIn: 10 });
-    ctx.body = {
-      token,
-      abody,
-    };
-  }
+
   async login() {
     const { ctx, app } = this;
     // const abody = ctx.state.user;
--- app/router.js
+++ app/router.js
@@ -6,7 +6,6 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
-  router.post('/jwttest', app.jwt, controller.member.phoneLogin.jwttest);
 
   router.post('/app/member/phoneLogin/bindPhone', controller.member.phoneLogin.bindPhone);// 1.1.3、 微信绑定手机号
   router.post('/app/member/info/reg', app.jwt, controller.member.info.reg);// 1.1.6、 注册后完善用户信息
