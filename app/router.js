4fc6d785f3f7b6d612ed8faab6b985871ac96b2e
Storh <storh1118@outlook.com>
2020-04-05 22:03:38 +0800

    1.2.1获取动态记录列表 初步完成未整理

--- app/router.js
+++ app/router.js
@@ -5,12 +5,14 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.member.phoneLogin.login);// 测试接口
+  router.prefix('/app');
+  router.post('/', controller.member.phoneLogin.login);// 测试接口
 
-  router.post('/app/member/phoneLogin/bindPhone', controller.member.phoneLogin.bindPhone);// 1.1.3、 微信绑定手机号
-  router.post('/app/member/info/reg', app.jwt, controller.member.info.reg);// 1.1.6、 注册后完善用户信息
+  router.post('/member/phoneLogin/bindPhone', controller.member.phoneLogin.bindPhone);// 1.1.3、 微信绑定手机号
+  router.post('/member/info/reg', app.jwt, controller.member.info.reg);// 1.1.6、 注册后完善用户信息
 
-  router.post('/app/member/info/getInfo', app.jwt, controller.member.info.getInfo);// 1.5.1、 获取用户基本信息
+  router.post('/content/main/getList', app.jwt, controller.content.main.getList);// 1.2.1、 获取动态记录列表
+  router.post('/member/info/getInfo', app.jwt, controller.member.info.getInfo);// 1.5.1、 获取用户基本信息
 
-  router.post('/app/baseData/distList', app.jwt, controller.baseData.distList);// 1.6.1、 获取街道小区列表
+  router.post('/baseData/distList', app.jwt, controller.baseData.distList);// 1.6.1、 获取街道小区列表
 };
