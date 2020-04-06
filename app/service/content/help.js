a7936a4a90d2c1ec39bfdc99d6f64c4b18573169
Storh <storh1118@outlook.com>
2020-04-07 00:10:23 +0800

    换个思路

--- app/service/content/help.js
+++ app/service/content/help.js
@@ -25,14 +25,12 @@ class HelpService extends Service {
 
                 ORDER BY a.regist_id DESC
                 ${limit}`;
-    const resultstr = await this.app.mysql.query(sqlstr);
-    const results = JSON.parse(JSON.stringify(resultstr));
-    const list = [];
-    results.forEach(element => {
-      if (element.headimgurl.length < 100) { element.headimgurl = this.app.config.publicAdd + element.headimgurl; }
-      if (element.add_time) { element.add_time = new Date(element.add_time).toLocaleString(); }
-      if (element.reply_time) { element.reply_time = new Date(element.reply_time).toLocaleString(); }
-      list.push(element);
+    const results = await this.app.mysql.query(sqlstr);
+    const list = results.map(item => {
+      if (item.headimgurl.length < 100) item.headimgurl = this.app.config.publicAdd + item.headimgurl;
+      if (item.add_time) item.add_time = new Date(item.add_time).toLocaleString();
+      if (item.reply_time) item.reply_time = new Date(item.reply_time).toLocaleString();
+      return item;
     });
     return list;
   }
--- app/service/content/pack.js
+++ app/service/content/pack.js
@@ -24,9 +24,9 @@ class PackService extends Service {
         ${limit}`;
     const results = await this.app.mysql.query(sqlstr);
     const list = results.map(item => {
-      if (item.headimgurl.length < 100) { item.headimgurl = this.app.config.publicAdd + item.headimgurl; }
-      if (item.add_time) { item.add_time = new Date(item.add_time).toLocaleString(); }
-      if (item.reply_time) { item.reply_time = new Date(item.reply_time).toLocaleString(); }
+      if (item.headimgurl.length < 100) item.headimgurl = this.app.config.publicAdd + item.headimgurl;
+      if (item.add_time) item.add_time = new Date(item.add_time).toLocaleString();
+      if (item.reply_time) item.reply_time = new Date(item.reply_time).toLocaleString();
       return item;
     });
     return list;
