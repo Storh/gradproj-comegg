63b688a4ecc52df7a30ba26c3ffcf70dbb12302a
Storh <storh1118@outlook.com>
2020-04-07 09:31:42 +0800

    获取图片和测试

--- app/controller/member/phoneLogin.js
+++ app/controller/member/phoneLogin.js
@@ -23,22 +23,36 @@ class PhoneLoginController extends Controller {
     // const token = app.jwt.sign({ foo: 'bar' }, app.config.jwt.secret, { expiresIn: 10 });
     // this.ctx.throw('有猫饼', { data: { token }, myErrType: 255 });
     // this.ctx.throw('有猫饼', { data: { token } });
-    const result = await this.app.mysql.get(this.app.config.dbprefix + 'user_profile', { user_id: 369, state: 1 });
-    const resultarr = [ result ];
-    const resultmap = resultarr.map(item => {
-      if (item.add_time) {
-        item.add_time = new Date(item.add_time).toLocaleString();
-      }
-      return item;
+
+    // const result = await this.app.mysql.get(this.app.config.dbprefix + 'user_profile', { user_id: 369, state: 1 });
+    // const resultarr = [ result ];
+    const resultarr = await this.app.mysql.select(app.config.dbprefix + 'upload_file_record', {
+      columns: [ 'src' ],
     });
+    const resultmap = resultarr.map(item => {
+      if (item.src) {
+        item.src = 'http://www.chinaclick.com.cn/ailin/' + item.src;
+      }
+      return item.src;
+    });
+    const str = resultmap.toString();
+    const link = str.replace(/,/g, `
+    `);
+
+    // const resultmap = resultarr.map(item => {
+    //   if (item.add_time) {
+    //     item.add_time = new Date(item.add_time).toLocaleString();
+    //   }
+    //   return item;
+    // });
 
     // console.log(result);
     const abody = await ctx.service.helper.getNameFirstCharter('W');
     ctx.body = {
       token,
       abody,
-      result,
-      resultmap,
+      // resultmap,
+      link,
     };
   }
 }
