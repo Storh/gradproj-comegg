456ccaad88319a2c7e75e055daabe50b3dd760a3
Storh <storh1118@outlook.com>
2020-04-14 11:47:54 +0800

    社区列表和小区列表选项sql语句修正

--- app/service/content/main.js
+++ app/service/content/main.js
@@ -292,13 +292,17 @@ AND file_id NOT IN (${photoIdArr.toString()})`;
 
     switch (reqData.district_type) {
       case 1:
-        sql_district_type = ' AND a.district_id = (SELECT district_id FROM ' + this.app.config.dbprefix + 'user_profile WHERE user_id = ' + user_id + ')';
+        sql_district_type = ` AND a.district_id = (SELECT district_id FROM ${this.app.config.dbprefix}user_profile WHERE user_id = ${user_id})`;
         break;
       case 2:
-        sql_district_type = 'AND FIND_IN_SET(a.district_id' +
-          '(SELECT GROUP_CONCAT(district_id) FROM ' + this.app.config.dbprefix + 'district WHERE is_delete = 0 AND state = 1' +
-          'AND parent_id = (SELECT parent_id FROM ' + this.app.config.dbprefix + 'district WHERE district_id = (SELECT district_id FROM ' + this.app.config.dbprefix + 'user_profile WHERE user_id = ' + user_id + '))))' +
-          'AND a.show_type = 2';
+        sql_district_type =
+        `
+        AND FIND_IN_SET(a.district_id,
+        (SELECT GROUP_CONCAT(district_id) FROM ${this.app.config.dbprefix}district WHERE is_delete = 0 AND state = 1
+        AND parent_id = (SELECT parent_id FROM ${this.app.config.dbprefix}district WHERE district_id = (SELECT district_id FROM ${this.app.config.dbprefix}user_profile WHERE user_id = ${user_id})))
+        )
+        AND a.show_type = 2
+        `;
         break;
       default:
         sql_district_type = '';
