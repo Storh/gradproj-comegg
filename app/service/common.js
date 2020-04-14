0284b227a43eff0b0f1c39ab8565d20383a2573a
Storh <storh1118@outlook.com>
2020-04-14 20:19:47 +0800

    获取用户爱好错误

--- app/service/common.js
+++ app/service/common.js
@@ -60,7 +60,7 @@ class CommonService extends Service {
   async getUserSpecHobyById(user_id, type) {
     let type_id;
     if (type === 'speciality') { type_id = 1; }
-    if (type === 'hobby') { type_id = 1; }
+    if (type === 'hobby') { type_id = 2; }
     const resultstr = await this.app.mysql.select(this.app.config.dbprefix + 'kind_relation', {
       where: { rel_id: user_id, type_id },
       columns: [ 'kind_id', 'kind_name' ],
